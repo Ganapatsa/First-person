dce962c3c1552f2a6c26c6545dfe91ed13f4660c
rganesh <rganesh1434@gmail.com>
2022-10-12 20:40:36 +0530

    First Program

---
+++ py2.py
@@ -0,0 +1,38 @@
+a=5
+b=6
+a+b
+11
+
+name = 'Ganapatsa'
+cell = 8880824232
+city = Hubballi
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    city = Hubballi
+NameError: name 'Hubballi' is not defined
+city = 'Hubballi'
+Full_Address = name+cell+city
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    Full_Address = name+cell+city
+TypeError: can only concatenate str (not "int") to str
+print(name)
+Ganapatsa
+print(name,cell,city)
+Ganapatsa 8880824232 Hubballi
+
+print('my name is ', name)
+my name is  Ganapatsa
+
+print('my cell number is ', cell)
+my cell number is  8880824232
+
+print('my city name is ', city)
+my city name is  Hubballi
+
+print('my name is ', name,'my cell number is ', cell,'my city name is ', city)
+my name is  Ganapatsa my cell number is  8880824232 my city name is  Hubballi
+
+cell=7795722558
+print(cell)
+7795722558
---
+++ py3.0.py
@@ -0,0 +1,28 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+name='ganesh'
+cell=7795722558
+city='hubballi'
+
+print(name,cell,city)
+ganesh 7795722558 hubballi
+print(name)
+ganesh
+
+print('my name is',name,'my cell number is',cell,'I am from',city)
+my name is ganesh my cell number is 7795722558 I am from hubballi
+
+print('my name is',name,  'my cell number is',cell,  'I am from',city)
+my name is ganesh my cell number is 7795722558 I am from hubballi
+print('my name is',name    ,'my cell number is',cell       ,'I am from',city)
+my name is ganesh my cell number is 7795722558 I am from hubballi
+
+print('my name is',name,"" 'my cell number is',cell,'I am from',city)
+my name is ganesh my cell number is 7795722558 I am from hubballi
+
+name,cell,city = 'Ganesh',7795722558,'hubballi'
+print(name,cell,city)
+Ganesh 7795722558 hubballi
+
+print('I am',name,'cell number is',cell,'I belongs to',city)
+I am Ganesh cell number is 7795722558 I belongs to hubballi
